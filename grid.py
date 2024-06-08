dba7cc7c05bbaa69e3709edf40ab6ecd33349d72
thistoolshallpath <sinewaiver@gmail.com>
2024-06-08 10:52:42 -0700

    Better assign_action functionality implemented

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
-class Grid:
+class Grid: #create midiversion and osc version?
     def __init__(self, rows, columns):
         self.buttons = {}
         self.rows = rows
@@ -7,6 +7,7 @@ class Grid:
         for y in range(rows):
             for x in range(columns):
                 self.buttons[(x, y)] = (Button(x, y))
+
 
     def __str__(self):
         grid_str = self.grid_str
@@ -19,9 +20,6 @@ class Grid:
             grid_str += '\n'
         return grid_str
 
-    # def __call__(self, x, y):
-    #     if y <= self.rows and x <= self.columns:
-    #         self.buttons[x, y]()
 
     def __getitem__(self, key):
         return self.buttons[key]
@@ -31,61 +29,66 @@ class Button:
     def __init__(self, x, y):
         self._state = False  # state is pressed or not
         self.pos = (x,y)
-        self._action = None
-        self._params = None
-        self.press_actions = [] # create actions/execution class?
-        self.press_params = []
-        self.release_actions = []
-        self.release_params = []
+        self._press_actions = [] # create actions/execution class?
+        self._press_params = []
+        self._release_actions = []
+        self._release_params = []
+
+    def get_actions(self, press: bool):
+        if press:
+            actions = self._press_actions
+            params = self._press_params
+        elif not press:
+            actions = self._release_actions
+            params = self._release_params
+        return actions, params
+
+    def assign_action(self, function, newparams, press:bool=True, override=False):
+        actions, params = self.get_actions(press)
+        params = self._press_params
+        print (params)
+        if len(actions) == 0 or override:
+            print(self.pos, "Assigning  single function :", function)
+            actions.append(function)
+            params.append(newparams)
+            print (self._press_actions, self._press_params)
+        else:
+            actions.append(function)
+            params.append(newparams)
+            print(self.pos, "now has an additional function")
+
+
+    def __call__(self, *args):
+        if not args: raise ValueError("no value for call press button: should be bool")
+        else:
+            press = args[0]
+            self.state = press
+            actions, params = self.get_actions(press)
+            ## Check for actions
+            if len(actions) > 0:
+                for i in range(len(actions)):
+                    actions[i](params[i])
+            else:
+                print("No assigned actions for button", self.pos)
+
 
     @property
     def state(self):
-        pressed = "pressed." if self._state == True else "unpressed"
-        print(self.pos, "is currently", pressed)
         return self._state
 
     @state.setter
-    def state(self, value):
-        pressed = "pressed." if self._state == True else "unpressed"
-        print(self.pos, "has been", pressed)
+    def state(self, value:bool):
         if self._state != value:
             self._state = value
+            # pressed = "pressed." if value else "unpressed"
+            # print(self.pos, "has been", pressed)
         else:
             raise ValueError("This is a redundant button-press")
 
-    def assign_action(self, function, params=None):
-        if self.num_actions == 0:
-            print("assigning function")
-            self._action = function
-        if self.num_actions == 0:
-            self._params = params
-        self.num_actions = 1
 
-
-    def __call__(self, value):
-        value = 127 if value == True else False
-        pressed = "pressed" if value == True else "released"
-        print (f"Button {self.pos} was {pressed}")
-        if self.num_actions == 1:
-            # params = self._params[0] if len(self._params) > 0 else None
-            self._action(self._params) #(self._params[0])
-        # elif self.num_actions > 1:
-        #     for i in range(len(self._action)):
-        #         self._action[i](self._params[i])
-        #         pass
-        else:
-            print("No assigned actions for button", self.pos)
-
-
-    def __del__(self):
-        if self._action:
-            self._action = None
-            print (f"actions for {self.pos} deleted")
-
-    def append_action(self, function):
-        pass
-
-    def __str__(self):
-        pressed = "pressed" if self._state else "not pressed"
-        print (f"Button {self.pos} is curently {pressed}")
+    # def __str__(self):
+    #     pressed = "pressed" if self._state else "not pressed"
+    #     print (f"Button {self.pos} is curently {pressed}")
+    #     # print ("Press functions: ", [i for i in self._press_actions])
+    #     # print ("Press functions: ", [i for i in self._release_actions])
 
--- main.py
+++ main.py
@@ -17,20 +17,34 @@ def test_midi_input(*args):
     else:
         midi_io.send_midi_message(midiout, note, vel)
 
+
 def midi_grid_mapping(*args):
     channel, note, vel = args[0][0]
+    x = note % 16
+    y = int(note / 16)
+    y = 7 - y
+    press = True if vel == 127 else False
+    print (vel)
+    KeyPad[x, y](vel)
 
+
+
+def basic(*args):
+    print (args)
 
 if __name__ == "__main__":
 
     # instantiate midi ports
     midiout = midi_io.open_virtual_output_port("Launchpad")
-    midi_io.create_port_callback("Launchpad", test_midi_input)
+    midi_io.create_port_callback("Launchpad", midi_grid_mapping)
+    KeyPad = grid.Grid(9, 9)
+    KeyPad.buttons[0,0].assign_action(print, "Hello World")
+    KeyPad.buttons[1,0].assign_action(test_midi_input, "Hello World")
 
-    KeyPad = grid.Grid(8, 8)
-    # KeyPad[0,0].assign_action(print, "Hello World")
-    # KeyPad[0,0].assign_action(midi_io.send_midi_message()
-    KeyPad[0,0](True)
+    KeyPad.buttons[0,0](True)
+    print(KeyPad)
+
+    ## launchpad instantiation of functionalities
 
 
     while True:
